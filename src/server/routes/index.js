cf7b9c7da15144fd66cdcd4441f29ecea2475184
bbouley <b.bouley@hotmail.co.uk>
2015-11-09 16:49:40 -0700

    added in new heroku tests

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Guest Speaker People' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
